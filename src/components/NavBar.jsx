5284772438b7ff4e1b6cfd697a581e0226f65da2
AlsoFabio <110996759+AlsoFabio@users.noreply.github.com>
2023-11-01 12:10:07 -0300

    se creo la barra de navegación

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Navbar() {
+
+    const navigate = useNavigate()
+
+    const rutas = [
+        { ruta: '/sites', nombre: "Sites" },
+        { ruta: '/people', nombre: "People" },
+    ]
+
+    const botonesNavegadores = (index, ruta, nombre) => {
+        return <button key={index} onClick={() => navigate(ruta)}>{nombre}</button>
+    }
+
+    return (
+        <nav>
+            {rutas.map((e, index) => (
+                botonesNavegadores(index, e.ruta, e.nombre)
+            ))}
+        </nav>
+    );
+}
+
+export default Navbar;
